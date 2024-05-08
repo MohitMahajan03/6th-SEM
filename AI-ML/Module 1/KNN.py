0b9b9bad6a85e91724833d1bd63ec64a0285ca35
MohitMahajan03 <mohitmah50@gmail.com>
2024-05-08 19:49:36 +0530

    Brute forced KNN

--- AI-ML/Module 1/KNN.py
+++ AI-ML/Module 1/KNN.py
@@ -1,9 +1,40 @@
+from collections import OrderedDict
+
 n = int(input("Enter the number of rows of data "))
 m = int(input("Enter the number of columns of data (without labels) "))
 
-data = [n][m]
+data = []
 
 print("Enter data row wise with spaces")
 
 for i in range(n):
-    data[i] = input()
+    data.append(input("").split(" "))
+
+data = [[int(j) for j in i] for i in data]
+
+print(data)
+data_labels = []
+data_labels = input("Enter the data labels with spaces ").split(" ")
+
+print(data_labels)
+
+if(n != len(data_labels)):
+    print("Invalid Input")
+
+else:
+    test_data = input("Enter the test data points with spaces").split(" ")
+    test_class = input("Enter the class the data belongs to ")
+    test_data = [int(i) for i in test_data]
+
+    dist = []
+    for i in range(n):
+        sums = 0
+        for j in range(m):
+            sums = sums + abs(test_data[j] - data[i][j])
+        dist.append([sums, data_labels[i]])
+
+    dist.sort(key = lambda x : x[0])    
+    print(dist)
+
+
+    
