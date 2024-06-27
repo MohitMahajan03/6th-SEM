4615935d110ba5dbbbca3bd4daee2f0d4c474538
MohitMahajan03 <mohitmah50@gmail.com>
2024-06-27 22:33:41 +0530

    Building lexer

--- SSCD/flashpy/basic.cpp
+++ SSCD/flashpy/basic.cpp
@@ -3,29 +3,95 @@
 #include<conio.h>
 #include<string.h>
 #include<unordered_map>
+#include<vector>
 
 using namespace std;
  
 unordered_map<string, string> tokens;
-
-class Token
-{
-
+unordered_map<char, string> operators = {
+    {'+' , "ADD"},
+    {'-' , "SUB"},
+    {'*' , "MUL"},
+    {'/' , "DIV"},
+    {'^' , "POW"},
 };
 
-class Lexer
-{
-
-};
 
 class Error
 {
     public:
 
-    string err(string text)
+    string err(char symbol)
     {
-        string error = "Unidentifiable symbol ";
-        error.append(text);
+        string error = "Unidentifiable symbol " + symbol;
         return error;
     }
 };
+
+class Lexer : public Error
+{
+    public:
+
+    unordered_map<string, string> tokenize(string text)
+    {
+        string digits, words;
+        bool isFloat = false;
+
+        for(auto &sym : text)
+        {
+            if(isdigit(sym))
+                digits += sym;
+                // cout<<"digits = "<<digits<<" sym = "<<sym<<endl;
+
+            else if(!isalnum(sym))
+            {
+                if(!digits.empty())
+                {
+                    if(isFloat == true)
+                        tokens[digits] = "FLOAT";
+                    else
+                        tokens[digits] = "INT";
+                    
+                    digits.clear();
+                }
+                switch(sym)
+                {
+                    case '+': tokens["+"] = operators[sym];
+                        break;
+                    case '-': tokens["-"] = operators[sym];
+                        break;
+                    case '*': tokens["*"] = operators[sym];
+                        break;
+                    case '/': tokens["/"] = operators[sym];
+                        break;
+                    case '^': tokens["^"] = operators[sym];
+                        break;
+                    default: err(sym);
+                }
+            }
+            else
+                err(sym);
+            }
+            if(!digits.empty())
+            {
+                if(isFloat == true)
+                    tokens[digits] = "FLOAT";
+                else
+                    tokens[digits] = "INT";
+                
+                digits.clear();
+            }
+        return tokens;
+    }
+};
+
+int main()
+{
+    Lexer l;
+    string text = "24+25";
+    unordered_map<string, string> res;
+    res = l.tokenize(text);
+    for(auto &it : res)
+        cout<<it.first<<" : "<<it.second<<endl;
+    return 0;
+}
