1b26c650bcf664840835faf124830bbc7a008353
MohitMahajan03 <mohitmah50@gmail.com>
2024-06-27 19:10:47 +0530

    Building flashpy

--- SSCD/flashpy/basic.cpp
+++ SSCD/flashpy/basic.cpp
@@ -1,8 +1,12 @@
 //'''TOKENS'''
 #include<iostream>
 #include<conio.h>
+#include<string.h>
+#include<unordered_map>
 
 using namespace std;
+ 
+unordered_map<string, string> tokens;
 
 class Token
 {
@@ -16,5 +20,12 @@ class Lexer
 
 class Error
 {
+    public:
 
+    string err(string text)
+    {
+        string error = "Unidentifiable symbol ";
+        error.append(text);
+        return error;
+    }
 };
