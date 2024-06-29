7ef70b48205f53795020e33f2a7a6414ea5c805b
MohitMahajan03 <mohitmah50@gmail.com>
2024-06-29 09:08:32 +0530

    building syntax tree

---
+++ Robotics and Automation/Module 3/module 3.md
@@ -0,0 +1,25 @@
+# 1. What are sensors? Explain the functions provided by sensors.
+# 2. Explain briefly the classification of sensors with a diagram.
+# 3. Explain briefly different types of internal sensors used in robots.
+# 4. Explain the following sensors used in robots.
+## Position sensors
+## Potentiometers
+## LVDT
+## Synchros and Resolver
+## Velocity sensors
+## Acceleration sensors
+## Force sensors
+# 5. Explain briefly the different types of external sensors used in robots
+# 6. What is a vision sensor? Explain the task performed by vision system in robots.
+# 7. Explain the elements in a vision system used in robots.
+# 8. Explain the steps involved in vision system
+# 9. Explain the hierarchy of a vision system
+# 10. What are the typical difficulties in a vision system.
+# 11. Explain some of the commonly used devices for signal conditioning
+# 12. Explain the following devices used for signal conditioning
+## Amplifiers
+## Filters
+## Modulators and demodulators
+## Analog and digital conversions
+## Bridge rectifiers
+## Signal analyse
--- SSCD/flashpy/syntree.cpp
+++ SSCD/flashpy/syntree.cpp
@@ -11,28 +11,62 @@ class Node
     public:
     string id;
     string val;
-    Node* left = NULL;
-    Node* right = NULL;
+    Node* left;
+    Node* right;
+
+    Node(string val, string id)
+    {
+        this->id = id;
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
 };
 Node* root = NULL;
 
 class Syntax
 {
 
+    private:
+    stack<pair<string, string>> stacks;
+    Node* op_node = NULL;
+    Node* num_node = NULL;
     public:
-
-    Node* new_node()
-    {
-        
-    }
 
     void syntree(vector<pair<string, string>>& exp)
     {
         for (auto& it : exp)
         {
-
+            if(it.second == "INT" || it.second == "FLOAT")
+            {
+                stacks.push(it);
+            }
+            else
+            {
+                op_node = new Node(it.first, it.second);
+                if(root == NULL)
+                {
+                    root = op_node;
+                    num_node = new Node(stacks.top().first, stacks.top().second);
+                    root->left = num_node;
+                    stacks.pop();
+                    num_node = new Node(stacks.top().first, stacks.top().second);
+                    root->right = num_node;
+                }
+                else
+                {
+                    op_node->left = root;
+                    num_node = new Node(stacks.top().first, stacks.top().second);
+                    stacks.pop();
+                    op_node->right = num_node;
+                    root = op_node;
+                }
+            }
         }
-        
     }
-
 };
+
+int main()
+{
+    return 0;
+}
